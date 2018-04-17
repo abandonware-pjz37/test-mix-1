fc16358ffe79260bf512df6ca31c54d1041a5a5e
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-04-17 14:15:59 +0300

    glog: BUILD_TESTING=OFF

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -8,3 +8,5 @@ set(
 foreach(submodule ${submodules})
   hunter_config(${submodule} GIT_SUBMODULE third_party/${submodule})
 endforeach()
+
+hunter_config(glog VERSION ${HUNTER_glog_VERSION} CMAKE_ARGS BUILD_TESTING=OFF)
