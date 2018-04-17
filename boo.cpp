ca498b192899a3b2a1d4eb8c189469b9ab8dcb41
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-04-17 23:49:11 +0300

    Optional folly download

--- boo.cpp
+++ boo.cpp
@@ -1,8 +1,11 @@
 #include <unicode/utypes.h>
 #include <rapidjson/reader.h>
 #include <websocketpp/version.hpp>
-#include <folly/Chrono.h>
 #include <duktape.h>
+
+#if !defined(SKIP_FOLLY)
+# include <folly/Chrono.h>
+#endif
 
 int main() {
 }
