c99d3d502d46b32b0c04a9d103d6e5061e82c474
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-04-17 21:20:13 +0300

    Enable folly

--- boo.cpp
+++ boo.cpp
@@ -1,7 +1,7 @@
 #include <unicode/utypes.h>
 #include <rapidjson/reader.h>
 #include <websocketpp/version.hpp>
-// #include <folly/Chrono.h>
+#include <folly/Chrono.h>
 #include <duktape.h>
 
 int main() {
