3742e73e5f0c2b2ffe431514c7a5bed7e93516f6
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-04-20 18:18:15 +0300

    Download WebKit

--- boo.cpp
+++ boo.cpp
@@ -7,5 +7,26 @@
 # include <folly/Chrono.h>
 #endif
 
+#if !defined(SKIP_WEBKIT)
+# include <cstring> // std::memmove
+
+# include <JavaScriptCore/JSBase.h>
+# include <JavaScriptCore/JSContextRef.h>
+# include <JavaScriptCore/JSObjectRef.h>
+# include <JavaScriptCore/JSStringRef.h>
+# include <JavaScriptCore/JSTypedArray.h>
+# include <JavaScriptCore/JSValueRef.h>
+# include <JavaScriptCore/JavaScript.h>
+# include <JavaScriptCore/WebKitAvailability.h>
+
+# include <wtf/Algorithms.h>
+# include <wtf/Assertions.h>
+# include <wtf/dtoa/double.h> // std::memmove is missing
+# include <wtf/persistence/PersistentDecoder.h>
+# include <wtf/text/ASCIIFastPath.h>
+# include <wtf/threads/BinarySemaphore.h>
+# include <wtf/unicode/UTF8.h>
+#endif
+
 int main() {
 }
--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -11,3 +11,35 @@ set(
 foreach(submodule ${submodules})
   hunter_config(${submodule} GIT_SUBMODULE third_party/${submodule})
 endforeach()
+
+set(enable_jit ON)
+if(ANDROID)
+  string(COMPARE EQUAL "${CMAKE_ANDROID_ARCH_ABI}" x86 _abi_x86)
+  string(COMPARE EQUAL "${CMAKE_ANDROID_ARCH_ABI}" armeabi-v7a _abi_arm)
+  string(COMPARE EQUAL "${CMAKE_ANDROID_ARCH_ABI}" arm64-v8a _abi_arm64)
+
+  if(_abi_x86)
+    set(enable_jit OFF)
+  elseif(_abi_arm)
+    set(enable_jit OFF)
+  elseif(_abi_arm64)
+    set(enable_jit ON)
+  else()
+    message(FATAL_ERROR "Unexpected: ${CMAKE_ANDROID_ARCH_ABI}")
+  endif()
+endif()
+
+hunter_config(
+    WebKit
+    GIT_SUBMODULE
+    third_party/WebKit
+    CMAKE_ARGS
+    CMAKE_BUILD_TYPE=Release CMAKE_CONFIGURATION_TYPES=Release
+
+    PORT=JSCOnly
+    ENABLE_TOOLS=OFF
+    ENABLE_API_TESTS=OFF
+    ENABLE_WEBCORE=OFF
+    ENABLE_WEBKIT=OFF
+    ENABLE_JIT=${enable_jit}
+)
