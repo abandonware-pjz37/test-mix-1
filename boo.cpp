7af404ace7496a6f5be0e86510861188062466cd
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-04-22 23:03:07 +0300

    Remove SKIP_PROTOBUF

--- boo.cpp
+++ boo.cpp
@@ -2,10 +2,7 @@
 #include <rapidjson/reader.h>
 #include <websocketpp/version.hpp>
 #include <duktape.h>
-
-#if !defined(SKIP_PROTOBUF)
-# include <google/protobuf/message.h>
-#endif
+#include <google/protobuf/message.h>
 
 #if !defined(SKIP_FOLLY)
 # include <folly/Chrono.h>
