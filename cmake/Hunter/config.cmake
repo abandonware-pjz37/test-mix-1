cf366e983f09fac11da3c3d34c0d4b8169f173f4
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-04-22 22:56:51 +0300

    Windows Store: Do not build protoc in Protobuf

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -4,10 +4,23 @@ set(
     websocketpp
     folly
     duktape
-    Protobuf
     glog
 )
 
 foreach(submodule ${submodules})
   hunter_config(${submodule} GIT_SUBMODULE third_party/${submodule})
 endforeach()
+
+if(is_windows_store OR IOS OR ANDROID)
+  set(protobuf_arg_protoc OFF)
+else()
+  set(protobuf_arg_protoc ON)
+endif()
+
+hunter_config(
+    Protobuf
+    GIT_SUBMODULE
+    third_party/Protobuf
+    CMAKE_ARGS
+    protobuf_BUILD_PROTOC=${protobuf_arg_protoc}
+)
