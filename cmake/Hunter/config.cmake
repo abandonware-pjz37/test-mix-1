bfc2fe20e876b0bb65f6d9a8509ee6e5fba23765
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-04-23 10:26:38 +0300

    Custom RapidJSON/Protobuf versions

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,26 +1,2 @@
-set(
-    submodules
-    RapidJSON
-    websocketpp
-    folly
-    duktape
-    glog
-)
-
-foreach(submodule ${submodules})
-  hunter_config(${submodule} GIT_SUBMODULE third_party/${submodule})
-endforeach()
-
-if(is_windows_store OR IOS OR ANDROID)
-  set(protobuf_arg_protoc OFF)
-else()
-  set(protobuf_arg_protoc ON)
-endif()
-
-hunter_config(
-    Protobuf
-    GIT_SUBMODULE
-    third_party/Protobuf
-    CMAKE_ARGS
-    protobuf_BUILD_PROTOC=${protobuf_arg_protoc}
-)
+hunter_config(RapidJSON VERSION 0.11-p2)
+hunter_config(Protobuf VERSION 2.4.1-p0)
