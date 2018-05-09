acef134b58651bd1cc16b67c84c0f16b8ce3d0b3
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-05-09 14:52:28 +0300

    Build with U_USING_ICU_NAMESPACE=ON

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,2 +1,8 @@
 hunter_config(RapidJSON VERSION 0.11-p2)
 hunter_config(Protobuf VERSION 2.4.1-p0)
+
+hunter_config(
+    ICU
+    VERSION ${HUNTER_ICU_VERSION}
+    CMAKE_ARGS U_USING_ICU_NAMESPACE=ON
+)
